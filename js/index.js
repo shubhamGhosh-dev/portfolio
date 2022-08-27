07564fa86f810e58302841dcd61a63c0af7c144e
Shubham Ghosh <shubhamghosh17@gmail.com>
2022-08-28 02:13:31 +0530

    adding send message functionality

--- js/index.js
+++ js/index.js
@@ -44,3 +44,81 @@ var glide2 = new Glide(".glide2", options);
 
 glide.mount();
 glide2.mount();
+//////////////////////////////////////////////////////////////////////////
+
+const emailInput = document.getElementById("email-input");
+const messageInput = document.getElementById("message-input");
+const messageForm = document.getElementById("message-form");
+const messageAlart = document.getElementById("message-alart");
+const msgSubmitBtn = document.getElementById("message-submit-btn"); 
+
+const sendIcon = document.getElementById("send-icon");
+const lodingIcon = document.getElementById("loading-icon");
+
+messageForm.addEventListener("submit", async (e) => {
+   e.preventDefault();
+
+   if((emailInput.value && validateEmailID(emailInput.value)) && messageInput.value){
+      disableBtn();
+      sendMessage(emailInput.value, messageInput.value)
+         .then(res => {
+            if(res.success){
+               emailInput.value = "";
+               messageInput.value = "";
+            }
+            enableBtn()
+            showAlert(res.msg);
+         })
+   }else{
+      if(emailInput.value && !validateEmailID(emailInput.value)){
+         showAlert("Please enter a valid email ID")
+      }else{
+         showAlert("Please fill the form appropriately.");
+      }
+   }
+})
+
+function showAlert(msg){
+   messageAlart.classList.add("active");
+   messageAlart.innerText = msg;
+
+   setTimeout(() => {
+      messageAlart.classList.remove("active");
+   }, 4500)
+}
+
+async function sendMessage(email, message){
+   const body = {
+      email: email,
+      message: message
+   }
+   const header = {
+      'Content-Type': 'application/json'
+   }
+
+   const options = {method: 'POST', headers: header, body: JSON.stringify(body)};
+
+   const res = await fetch('https://oauth-messaging-server.herokuapp.com/sendmail', options).then(response => response.json())
+   return res;
+}
+
+function validateEmailID(email){
+   const REG_EXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   if(REG_EXP.test(email)){
+       return true;
+   }
+   return false;
+}
+
+function disableBtn(){
+   msgSubmitBtn.setAttribute("disabled", true);
+   msgSubmitBtn.classList.add("disable-send-btn")
+   sendIcon.classList.add("disabled");
+   lodingIcon.classList.remove("disabled");
+}
+function enableBtn(){
+   msgSubmitBtn.removeAttribute("disabled");
+   msgSubmitBtn.classList.remove("disable-send-btn")
+   sendIcon.classList.remove("disabled");
+   lodingIcon.classList.add("disabled");
+}
